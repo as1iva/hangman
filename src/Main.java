ca4eab754bdb45481a49f1e91d06bcb8ec894f12
Tim Fadeev <1mailforwork1@gmail.com>
2024-07-10 22:39:49 +0300

    feat: add get word method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    public static char[] getWord() {
+        List<String> wordsFile = null;
+        Random random = new Random();
+
+        try {
+            wordsFile = Files.readAllLines(Paths.get("russian-words.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        char[] word = wordsFile.get(random.nextInt(0, wordsFile.size())).toCharArray();
+
+        return word;
+    }
+
     public static void main(String[] args) {
         do {
             System.out.println("1 - Новая игра\n" +
