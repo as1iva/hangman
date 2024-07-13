ca8fb466aa004ad3b7099d0e4ae61997488ed0fa
Tim Fadeev <1mailforwork1@gmail.com>
2024-07-13 18:49:56 +0300

    feat: add is letter correct method

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
     public static Random random = new Random();
     public static int mistakes = 0;
+    public static List<Character> letters = new ArrayList<>();
     public static int MISTAKES_TO_LOSE = 6;
     public static String GAME_STATE_PLAYER_WON = "Вы победили!\n";
     public static String GAME_STATE_PLAYER_LOSE = "Вы проиграли!\n";
@@ -18,8 +16,6 @@ public class Main {
 
     public static char[] getWord() {
         List<String> wordsFile = null;
-
-
         try {
             wordsFile = Files.readAllLines(Paths.get("russian-words.txt"));
         } catch (IOException e) {
@@ -137,6 +133,32 @@ public class Main {
         }
     }
 
+    public static boolean isLetterCorrect(Character guess) {
+        String regex = "^[а-яё]+$";
+        if (!guess.toString().matches(regex)) {
+            System.out.println("Можно вводить только кириллицу в нижнем регистре.");
+            return false;
+        }
+
+        if (letters.isEmpty()) {
+            letters.add(guess);
+            System.out.println(letters);
+            return true;
+        }
+
+        for (Character letter : letters) {
+            if (guess.equals(letter)) {
+                System.out.println("Эта буква была введена раннее, попробуйте другую!");
+                return false;
+            }
+        }
+        letters.add(guess);
+
+        Collections.sort(letters);
+        System.out.println(letters);
+        return true;
+    }
+
     public static String checkGameState(char[] maskWord, int mistakes) {
         if (mistakes == MISTAKES_TO_LOSE) {
             printHangman(MISTAKES_TO_LOSE);
@@ -164,13 +186,16 @@ public class Main {
         do {
             System.out.print("Введите букву: ");
             char guess = scanner.next().charAt(0);
+            if (!isLetterCorrect(guess)) {
+                continue;
+            }
             checkWord(guess, word, maskWord);
-
             String gameState = checkGameState(maskWord, mistakes);
 
             if (!Objects.equals(gameState, GAME_STATE_NOT_FINISHED)) {
                 System.out.println(gameState);
                 mistakes = 0;
+                letters.clear();
                 return;
             }
         } while (true);
